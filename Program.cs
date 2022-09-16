0e9fbd1d1f302e60be1a4212a844b2a219eac48f
milina-s <milina.samokhatnyaya@gmail.com>
2022-09-16 15:49:34 +0200

    create classes (entities)

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            List<Auto> autos = Data.autos;
+            List<Client> clients = Data.clients;
+            List<Check> checks = Data.checks;
+
+            
+        }
+
+    }
+
+}
