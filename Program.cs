4fe26f61cbaa127e82954a45d4f593fe6aa5fa35
milina-s <milina.samokhatnyaya@gmail.com>
2022-09-18 12:36:20 +0200

    1-2 queries

--- Program.cs
+++ Program.cs
@@ -15,7 +15,41 @@ namespace lab1
             List<Client> clients = Data.clients;
             List<Check> checks = Data.checks;
 
-            
+            // 1
+            Console.WriteLine("\n1. Список клієнтів та машин, що вони брали на прокат: ");
+
+            var q1 = from ch in checks
+                     join a in autos on ch.AutoId equals a.Id
+                     join cl in clients on ch.ClientId equals cl.Id
+                     group a by cl;
+
+
+            foreach (var group in q1)
+            {
+                Console.WriteLine($"{group.Key.Name} {group.Key.Lastname}: ");
+                foreach (var el in group)
+                {
+                    Console.WriteLine($"   {el}");
+                }
+            }
+
+            // 2
+            Console.WriteLine("\n2. Моделі машин, згруповані за брендом:");
+
+            var q2 = from a in autos
+                     orderby a.Model, a.Brand
+                     group a by a.Brand;
+
+            foreach (var group in q2)
+            {
+                Console.WriteLine($"\nБренд: {group.Key}");
+                foreach (var el in group)
+                {
+                    Console.WriteLine($"  {el.Model}");
+                }
+            }
+
+
         }
 
     }
